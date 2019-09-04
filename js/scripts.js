4112e1183c15ee80781256faec5520a960a65db8
michael beloff <michael.a.beloff@gmail.com>
2019-09-04 11:34:22 +1000

    layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,49 @@
 $(document).ready(function () {
-	Waves.attach('.btn, .btn-floating', ['waves-light']);
-	// Waves.attach('.view .mask', ['waves-light']);
-	Waves.attach('.waves-light', ['waves-light']);
-	Waves.attach('.navbar-nav a, .nav-icons li a, .navbar form, .nav-tabs .nav-item', ['waves-light']);
-	Waves.attach('.navbar-brand', ['waves-light']);
-	Waves.attach('.pager li a', ['waves-light']);
-	Waves.attach('.pagination .page-item .page-link', ['waves-effect']);
-	Waves.init();//Preloading script
+  Waves.attach('.btn, .btn-floating', ['waves-light']);
+  // Waves.attach('.view .mask', ['waves-light']);
+  Waves.attach('.waves-light', ['waves-light']);
+  Waves.attach('.navbar-nav a, .nav-icons li a, .navbar form, .nav-tabs .nav-item', ['waves-light']);
+  Waves.attach('.navbar-brand', ['waves-light']);
+  Waves.attach('.pager li a', ['waves-light']);
+  Waves.attach('.pagination .page-item .page-link', ['waves-effect']);
+  Waves.init(); //Preloading script
 });
 
 new WOW().init();
 
 $('.jarallax').jarallax();
 
-$(window).scroll(function(){
+$(window).scroll(function () {
   parallax();
 })
 
 function parallax() {
   var wScroll = $(window).scrollTop();
 
-  $('.parallax--box').css('top', 
-  (wScroll*0.02)+'em');
+  $('.parallax--box').css('top',
+    (wScroll * 0.02) + 'em');
 }
 
+$(document).ready(function () {
 
+  
 
+  $(".btn-cat").click(function () {
+    $(".cat").removeClass("show");
+    $(".btn-cat").removeClass("btn-active");
+    $(this).addClass("btn-active");
+  });
+
+  $("#van-link").click(function () {
+    $("#van").addClass("show");
+  });
+
+  $("#awd-link").click(function () {
+    $("#awd").addClass("show");
+  });
+
+  $("#car-link").click(function () {
+    $("#car").addClass("show");
+  });
+
+});
